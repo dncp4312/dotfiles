ce33ef6709f7e32b078564f6f355134e21378ab0
dncp4312 <24888745+dncp4312@users.noreply.github.com>
2021-06-14 07:49:53 +0530

    Aliases update v0.6

--- zsh/.aliases.zsh
+++ zsh/.aliases.zsh
@@ -2,6 +2,13 @@
 
 alias x=exit
 alias c=clear
+alias ea="vim ~/.zsh/.aliases.zsh"
+alias ep="vim ~/.zprofile"
+alias ez="vim ~/.zshrc"
+alias es="vim ~/.config/sxhkd/sxhkdrc"
+alias eb="vim ~/.config/bspwm/bspwmrc"
+alias ec="vim ~/.config/picom/picom.conf"
+alias er="vim ~/.config/polybar/config.ini"
 
 alias pQ='pacman -Q'
 alias pT='pactree -d 1'
@@ -16,7 +23,7 @@ alias pCC='paccache -rk1 -ruk0'
 alias gpref='nano /etc/default/grub'
 alias gcnfup='grub-mkconfig -o /boot/grub/grub.cfg'
 
-alias sys='sudo sys'
+alias sys='sudo systemctl'
 alias sysrun='sudo systemctl start'
 alias sysstp='sudo systemctl stop'
 alias sysres='sudo systemctl restart'
@@ -38,8 +45,20 @@ alias cmatrix='cmatrix ; c'
 alias theme='/home/xevil/.ricing/terminaltheme.sh'
 
 alias gs='c ; git status'
-alias gl='c ; git log'
+alias gl='c ; git log --oneline'
 alias gc='git commit -c'
 alias gr='git reset HEAD --hard'
+alias gt='git blot'
+alias gh='git glo'
+alias gd='git checkout'
 
 alias mi='micro'
+
+alias d='docker'
+alias di='docker images'
+alias dcu='docker-compose up -d'
+alias dcd='docker-compose down'
+
+alias k='xdotool k'
+alias ku='xdotool keyup'
+alias kd='xdotool keydown'
