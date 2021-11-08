5cba122626e07225668a0decbc57422fbc5ee9f3
dncp4312 <24888745+dncp4312@users.noreply.github.com>
2021-11-08 07:48:29 +0530

    Aliases update v0.6.4

--- zsh/.aliases.zsh
+++ zsh/.aliases.zsh
@@ -1,7 +1,23 @@
+#Temporary
+alias hb="cd ~/Projects/hta_be ; rails c"
+alias rs='clear ; bundle exec rspec'
+alias be='bundle exec'
+alias ruby='c;ruby'
+
 #Aliases
 
 alias x=exit
 alias c=clear
+
+alias ls='exa --group-directories-first'
+alias ll='ls -l'
+alias la='ls -la'
+alias grep=rg
+alias cat=bat
+#alias cat=ccat
+alias lat=lolcat
+alias yd="youtube-dl -o '~/Videos/%(title)s-%(id)s.%(ext)s"  
+
 alias ea="vim ~/.zsh/.aliases.zsh"
 alias ep="vim ~/.zprofile"
 alias ez="vim ~/.zshrc"
@@ -20,6 +36,7 @@ alias pR='sudo pacman -Rns'
 alias yS='yay -S --nodiffmenu'
 alias rmorph='pacman -Qtdq | sudo pacman -Rns -'
 alias pCC='paccache -rk1 -ruk0'
+alias pU='sudo pacman -Syu'
 
 alias gpref='nano /etc/default/grub'
 alias gcnfup='grub-mkconfig -o /boot/grub/grub.cfg'
@@ -29,20 +46,21 @@ alias sysrun='sudo systemctl start'
 alias sysstp='sudo systemctl stop'
 alias sysres='sudo systemctl restart'
 
+alias connected='c ; nmap -sP 192.168.1.0/24'
 alias running='systemctl --type=service --state=running'
 alias enabled='systemctl list-unit-files | grep enabled'
-alias shutnw='shutdown now'
+alias shutnw='sudo shutdown now'
+alias suspend='sudo systemctl suspend'
 
 alias ipl='ip addr | grep wlan0 | cut -d " " -f6 | grep /'
 alias ipp='curl ifconfig.me'
-alias chngcol='python3 ~/.local/share/plasma/desktoptheme/breeze-alphablack/desktoptheme.py set theme.accentColor'
-alias yd=youtube-dl
 
 alias rcup='rcup -v'
 alias mkrc='mkrc -v'
 alias lsrc='lsrc -v'
 
-alias cmatrix='cmatrix ; c'
+#alias cmatrix='cmatrix ; c'
+alias matrix="unimatrix -n -s 94 -l 'o' -f | lolcat"
 alias theme='/home/xevil/.ricing/terminaltheme.sh'
 
 alias gs='c ; git status'
@@ -52,8 +70,6 @@ alias gr='git reset HEAD --hard'
 alias gt='git blot'
 alias gh='git glo'
 alias gd='git checkout'
-
-alias mi='micro'
 
 alias d='docker'
 alias di='docker images'
@@ -66,3 +82,4 @@ alias kd='xdotool keydown'
 
 alias bq='bspc quit'
 alias br='bspc wm -r'
+
