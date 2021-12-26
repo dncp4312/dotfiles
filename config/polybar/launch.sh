1c15be1e64f8202c406694af6ca0cb6969ad55bf
dncp4312 <24888745+dncp4312@users.noreply.github.com>
2021-12-27 05:20:16 +0530

    Polybar update v0.2

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -8,10 +8,10 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 if [ $(xrandr --query | grep " connected" | wc -l) -gt 1  ]; then
 	  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
 		      #MONITOR=$m polybar --reload main &
-		      MONITOR=HDMI-3 polybar --reload left &
-		      MONITOR=DP-2 polybar --reload right &
+		      MONITOR=HDMI-3 polybar left &
+		      MONITOR=DP-2 polybar right &
 			    done
 else
-	  polybar --reload main &
+	  polybar main &
 fi
 
