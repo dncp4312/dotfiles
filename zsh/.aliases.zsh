96c94b421fc25d598a7b0bc13eb5ca0b3430f4eb
dncp4312 <24888745+dncp4312@users.noreply.github.com>
2022-01-25 15:42:29 +0530

    Aliases update v0.6.6

--- zsh/.aliases.zsh
+++ zsh/.aliases.zsh
@@ -3,17 +3,26 @@ alias hb="cd ~/Projects/hta_be ; rails c"
 alias rs='clear ; bundle exec rspec'
 alias be='bundle exec'
 alias ruby='c;ruby'
+alias ht="cd ~/Projects/hta_be"
+alias cr="cd ~/.rbenv/versions/2.7.4/lib/ruby/2.7.0"
+alias gems="cd ~/.rbenv/versions/2.7.4/lib/ruby/gems/2.7.0/gems ; c"
+alias rc="rails c"
+alias rt="rails c -e test"
+alias sk="be sidekiq"
+alias -s rb=vim
 
 #Aliases
 
 alias x=exit
 alias c=clear
+alias v=vim
+alias su='sudo -i'
 
 alias ls='exa --group-directories-first'
 alias ll='ls -l'
-alias la='ls -la'
-alias grep=rg
-alias cat=bat
+alias la='ls -lag'
+# alias grep=rg
+# alias cat=bat
 #alias cat=ccat
 alias lat=lolcat
 alias yd="youtube-dl -o '~/Videos/%(title)s-%(id)s.%(ext)s"  
@@ -45,6 +54,7 @@ alias sys='sudo systemctl'
 alias sysrun='sudo systemctl start'
 alias sysstp='sudo systemctl stop'
 alias sysres='sudo systemctl restart'
+alias syssta='systemctl status'
 
 alias connected='c ; nmap -sP 192.168.1.0/24'
 alias running='systemctl --type=service --state=running'
@@ -59,20 +69,20 @@ alias rcup='rcup -v'
 alias mkrc='mkrc -v'
 alias lsrc='lsrc -v'
 
-#alias cmatrix='cmatrix ; c'
-alias matrix="unimatrix -n -s 94 -l 'o' -f | lolcat"
-alias theme='/home/xevil/.ricing/terminaltheme.sh'
-
 alias gs='c ; git status'
 alias gl='c ; git log --oneline'
-alias gc='git commit -c'
+alias gc='git commit -m'
 alias gr='git reset HEAD --hard'
 alias gt='git blot'
 alias gh='git glo'
 alias gd='git checkout'
+alias gsm='git stash push -m'
+alias gsp='git stash pop'
+alias gsl='git stash list'
+alias gsa='git stash apply --index'
 
 alias d='docker'
-alias di='docker images'
+alias di='c;docker images'
 alias dcu='docker-compose up -d'
 alias dcd='docker-compose down'
 
@@ -83,3 +93,7 @@ alias kd='xdotool keydown'
 alias bq='bspc quit'
 alias br='bspc wm -r'
 
+alias theme='/home/xevil/.ricing/terminaltheme.sh'
+alias matrix="unimatrix -n -s 94 -l 'o' -f | lolcat"
+alias discord="LIBVA_DRIVER_NAME=i915 discord --ignore-gpu-blocklist --disable-features=UseOzonePlatform --enable-feature=VaapiVideoDecoder --use-gl=desktop --enable-gpu-rasterization --enable-zero-copy"
+alias fzf="fzf --height=80% --layout=reverse-list --border=horizontal --no-info --preview='bat --color=always --style=numbers {}' --color='border:magenta,prompt:magenta,hl:magenta,hl+:white,pointer:white,marker:white,bg+:magenta,gutter:grey'"
