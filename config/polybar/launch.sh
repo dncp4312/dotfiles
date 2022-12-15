f5cddb1105cf69e31a21cb8f23ffbd64163f1c9c
dncp4312 <24888745+dncp4312@users.noreply.github.com>
2022-12-16 00:34:07 +0530

    Polybar update v0.4.4

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -6,12 +6,18 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 if [ $(xrandr --query | grep " connected" | wc -l) -gt 1  ]; then
-	  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-		      #MONITOR=$m polybar --reload main &
-		      MONITOR=HDMI-3 polybar left &
-		      MONITOR=DP-2 polybar right &
-			    done
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+	#MONITOR=$m polybar --reload main &
+	MONITOR=HDMI-3 polybar left &
+	MONITOR=DP-2 polybar right &
+    done
 else
-	  polybar main &
+    case "$(xrandr --query --verbose | grep eDP-1 | cut -d' ' -f6)" in
+	'normal')  bar='main' ;;
+	'inverted')  bar='main' ;;
+	'right')  bar='portrait' ;;
+	'left')  bar='portrait' ;;
+    esac
+    polybar $bar &
 fi
 
