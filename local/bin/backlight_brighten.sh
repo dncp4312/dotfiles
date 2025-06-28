d2121470f4c5f500a3f048c9512f3fbc87ad91e4
dncp4312 <24888745+dncp4312@users.noreply.github.com>
2025-06-28 18:10:00 +0530

    Scripts update v0.2.2

--- local/bin/backlight_brighten.sh
+++ local/bin/backlight_brighten.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 max=$(cat /sys/class/backlight/amdgpu_bl1/max_brightness)
-curr=$(cat /sys/class/backlight/amdgpu_bl1/actual_brightness)
+curr=$(cat /sys/class/backlight/amdgpu_bl1/brightness)
 div=$((max / 255 + curr / 5))
 next=$((curr+div))
 
 [[ $next -lt $max ]] && curr=$next || curr=$max
 
-echo $curr  > /sys/class/backlight/amdgpu_bl1/brightness
+echo $curr > /sys/class/backlight/amdgpu_bl1/brightness
--- local/bin/backlight_dim.sh
+++ local/bin/backlight_dim.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 max=$(cat /sys/class/backlight/amdgpu_bl1/max_brightness)
-curr=$(cat /sys/class/backlight/amdgpu_bl1/actual_brightness)
+curr=$(cat /sys/class/backlight/amdgpu_bl1/brightness)
 div=$((max / 255 + curr / 5))
 next=$((curr-div))
 
 [[ $next -gt 0 ]] && curr=$next || curr=0
 
-echo $curr  > /sys/class/backlight/amdgpu_bl1/brightness
+echo $curr > /sys/class/backlight/amdgpu_bl1/brightness
